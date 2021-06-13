7ee3545fa247f9bcc586bcec81937ce053eba44e
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2021-06-14 00:30:55 +0200

    Update Polyg_CMIP5_daily_V1.0.js

--- Polyg_CMIP5_daily_V1.0.js
+++ Polyg_CMIP5_daily_V1.0.js
@@ -1,38 +1,55 @@
-// specify start and end date
-var startDate = ee.Date('2035-01-01');
-var endDate = ee.Date('2042-01-01');
+//SCRIPT FOR EXTRACTION AND DOWNLOADING CMIP5 GCM's MODEL OUTPUTS AT DAILY SCALE//// 
+//////NASA-NEX GDDP Earth Exchange Global Daily Downscaled Climate Projections /////
+////////////////////////////////////////////////////////////////////////////////////
 
-// get the dataset between date range and extract band on interest
+//INSTRUCTIONS////
+//A shapefile is required as an asset. 
+//Select the polygon of interest according to the attributes of the asset in line 32.
+//Choose "pr", "tasmax" or "tasmin" in meteo variable of line 24.
+//Preferably, choose an appropiate name when exporting daily data to Google Drive.
+//Historical data is available from 1950 to 2005 and RCP 4.5-8.5 from 2006 to 2099.
+//When plotting on the console (line 67), only a few years can be used.
+//More information, please check: 
+//https://developers.google.com/earth-engine/datasets/catalog/NASA_NEX-GDDP
+
+
+// Firstly, you have to specify start and end date.
+var startDate = ee.Date('2022-01-01');
+var endDate = ee.Date('2023-12-31');
+
+// Calling the collection
 var dataset = ee.ImageCollection('NASA/NEX-GDDP')
                   .filter(ee.Filter.date(startDate,endDate));
-var maximumAirTemperature = dataset.select('tasmax');
+//Selecting the meteorological variable of interest.
+var meteo = dataset.select('tasmax');
 
-// get projection information
-var proj = maximumAirTemperature.first().projection();
+// Geting projection information
+var proj = meteo.first().projection();
 
+//Including the asset
 var tabla = ee.FeatureCollection('users/jmontenegrog/RegPeru_geoWGS84');
-//var point = tabla.first().geometry();
 
-var point = tabla.filter(ee.Filter.eq('NOMBDEP', 'PIURA')).geometry()
+//Filtering polygon, you must select the specific attribute
+var point = tabla.filter(ee.Filter.eq('NOMBDEP','PIURA')).geometry();
 
-// calculate number of days to map and extract data for
+//Total amount of days to map and extract data
 var n = endDate.difference(startDate,'day').subtract(1);
 
-// map over each date and extract all climate model values
+// Mapping over each date to extract daily data
 var timeseries = ee.FeatureCollection(
   ee.List.sequence(0,n).map(function(i){
     var t1 = startDate.advance(i,'day');
     var t2 = t1.advance(1,'day');
     var feature = ee.Feature(point);
-    var dailyColl = maximumAirTemperature.filterDate(t1, t2);
+    var dailyColl = meteo.filterDate(t1, t2);
     var dailyImg = dailyColl.toBands();
-    // rename bands to handle different names by date
+    // Renaming and handling names
     var bands = dailyImg.bandNames();
     var renamed = bands.map(function(b){
       var split = ee.String(b).split('_');
       return ee.String(split.get(0)).cat('_').cat(ee.String(split.get(1)));
     });
-    // extract the data for the day and add time information
+    // Daily extraction and adding time information
     var dict = dailyImg.rename(renamed).reduceRegion({
       reducer: ee.Reducer.mean(),
       geometry: point,
@@ -40,22 +57,36 @@ var timeseries = ee.FeatureCollection(
     }).combine(
       ee.Dictionary({'system:time_start':t1.millis(),'isodate':t1.format('YYYY-MM-dd')})
     );
-    return ee.Feature(point,dict);
+    return ee.Feature(null,dict);
   })
 );
 print(timeseries);
 
-// get properties to chart (all climate models)
+// Geting properties to chart for the 21 GCM`s
 var props = timeseries.first().propertyNames().removeAll(['system:time_start','system:index','isodate']);
 
-// Make a chart of the results.
-var chart = ui.Chart.feature.byFeature(timeseries, 'system:time_start', props.getInfo());
-print(chart);
+// ACTIVATE IN CASE OF PLOTTING IN GEE CONSOLE
+//var chart = ui.Chart.feature.byFeature(timeseries, 'system:time_start', props.getInfo());
+//print(chart);
 
+//Ploting selected polygon according to line 32
 Map.addLayer(point);
 Map.centerObject(point,2);
 
-// export feature collection to CSV
+//ACTIVATE IN CASE OF REMOVING A FEATURE PROPERTY (GCM, date "isodate", "system:time_start" and others)
+// Generic Function to remove a property from a feature
+//var removeProperty = function(feat, property) {
+//  var properties = feat.propertyNames()
+//  var selectProperties = properties.filter(ee.Filter.neq('item', property))
+//  return feat.select(selectProperties)
+//}
+// remove property color in each feature
+//var ts = timeseries.map(function(feat) {
+//  return removeProperty(feat, 'isodate')
+//})
+
+
+// Finally, time to export the extracted data to a csv file!
 Export.table.toDrive({
   collection: timeseries,
   description: 'NEX-GDDP-timeseries',
